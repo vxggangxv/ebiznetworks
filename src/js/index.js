73cbe9c55ff5f73205e92ff6b504c9a1b4588570
Hye jun <vxggangxv@naver.com>
2017-02-08 22:10:25 +0900

    추가수정

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(window).scroll(function(){
+		var st = $(window).scrollTop();
+
+		if(st > 12){
+			$("#i_logo").css("display", "none");
+		}else {
+			$("#i_logo").css("display", "block");
+		}
+	});
+});
