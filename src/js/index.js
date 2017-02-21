9ab2a4e1fb190d3148c6fa61be45c183a1df825c
Hye jun <vxggangxv@naver.com>
2017-02-21 10:34:36 +0900

    고정텍스트수정

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$(window).scroll(function(){
+	/*$(window).scroll(function(){
 		var st = $(window).scrollTop();
 
 		if(st > 12){
@@ -7,5 +7,5 @@ $(document).ready(function() {
 		}else {
 			$("#i_logo").css("display", "block");
 		}
-	});
+	});*/
 });
